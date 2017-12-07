0ce8e2fd320c6dac61746295317f4e04161b5a3f
Linzuo Li <linzuolee@gmail.com>
2017-12-06 17:11:18 -0800

    reorganized structure, code for submission

--- src/lgbm_train.py
+++ src/lgbm_train.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import lightgbm as lgb
-import csv
 import time
 from datetime import datetime, timedelta
 DIR = '../data/'
@@ -9,36 +8,35 @@ DIR = '../data/'
 start = time.time()
 
 print("GETTING TRAINING FEATURES...")
-train_x = pd.read_csv(DIR + 'train_x_features.csv')
+train_x = pd.read_csv(DIR + 'processed/train_features.csv')
 print("\tDone.")
 
 print("GETTING TRAINING LABELS...")
-train_labels = pd.read_csv(DIR + 'label_train_features.csv',
-                           names = ['num', 'reorder'])
-train_labels = train_labels[train_labels.columns[1]]
+train_labels = train_x['reordered']
 print("\tDone.")
 
 print("GETTING TESTING FEATURES...")
-test_x = pd.read_csv(DIR + 'test_x_features.csv')
+test_x = pd.read_csv(DIR + 'processed/test_features.csv')
 print("\tDone.")
 
 features = ['order_dow', 'order_hour_of_day', 'days_since_prior_order',
         'reorder_rate', 'order_total','avg_add_to_cart_order', 'day_count', 'hour_count',
         'reorder_total', 'orders_sum', 'days_since_prior_std','avg_basket', 'avg_reorder', 'num_unique_items',
-        'aisle_id', 'department_id', 'up_orders', 'up_reorders', 'up_reorder_rate', 'up_add_to_cart_order', 
-        'up_days_since_prior_order', 'order_ratio', 'delta_dow', 'delta_order_hour_of_day', 'ordered_last_time', 
-        'reorder_total_ratio', 'reorder_total_ratio', 'numbers_since_last_order', 'first_ordered_number',
+        'aisle_id', 'department_id', 'up_orders', 'up_reorders', 'up_reorder_rate', 'up_add_to_cart_order',
+        'up_days_since_prior_order', 'order_ratio', 'delta_dow', 'delta_order_hour_of_day', 'ordered_last_time', 'aisle_orders', 'depart_orders',
+        'numbers_since_last_order', 'first_last', 'first_ordered_number', 'ratio_since_first_purchase', 'bought_times',
         'comp_size', 'avg_diff', 'std_diff']
 #'comp_size', 'avg_diff', 'std_diff'
 
-# parameter for lgbt
+# parameter for lgbt#0.38119
 params = {
     'task': 'train',
     'boosting_type': 'gbdt',
     'objective': 'binary',
     'metric': {'binary_logloss'},
-    'num_leaves': 96,
-    'max_depth': 10,
+    'max_depth': 5,
+    'learning_rate': 0.05,
+    'num_leaves': 32,
     'feature_fraction': 0.9,
     'bagging_fraction': 0.95,
     'bagging_freq': 5
@@ -59,6 +57,6 @@ print('Predicting......')
 pred = bst.predict(test_x[features])
 print('Prediction Done......')
 test_x['confidence'] = pred
-test_x[['order_id', 'product_id', 'confidence']].to_csv('lgbm_pred.csv', mode='w+', index=False)
+test_x[['order_id', 'product_id', 'confidence']].to_csv(DIR + 'predict/lgbm_pred.csv', mode='w+', index=False)
 end = time.time()
 print(str((end - start) / 60), "minutes")
